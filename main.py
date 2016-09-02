3ad0313eb186d80ed71f1e525e230fcfc3728d64
Kyle <kylewebb58@yahoo.com>
2016-09-01 20:19:35 -0500

    done with signup

--- main.py
+++ main.py
@@ -17,8 +17,8 @@
 import os
 import re
 import webapp2
-import jinja2
-from google.appengine.ext import db
+
+
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
@@ -32,35 +32,34 @@ EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
-class Index(webapp2.RequestHandler):
-    form = """
+form = """
     <form method="post">
                 <table>
                     <tr>
-                        <td><label for="username">Username</label></td>
+                        <td><label for="username">username</label></td>
                         <td>
-                            <input name="username" type="text" value="" required>
+                            <input name="username" type="text" value="%(username)s"/>%(error_username)s
                             <span class="error"></span>
                         </td>
                     </tr>
                     <tr>
                         <td><label for="password">Password</label></td>
                         <td>
-                            <input name="password" type="password" required>
+                            <input name="password" type="password" value="%(password)s"/>%(error_password)s
                             <span class="error"></span>
                         </td>
                     </tr>
                     <tr>
                         <td><label for="verify">Verify Password</label></td>
                         <td>
-                            <input name="verify" type="password" required>
+                            <input name="verify" type="password" value="%(verify)s"/>%(error_verify)s
                             <span class="error"></span>
                     </td>
                     </tr>
                     <tr>
                         <td><label for="email">Email (optional)</label></td>
                         <td>
-                            <input name="email" type="email" value="">
+                            <input name="email" type="email" value="%(email)s"/>%(error_email)s
                             <span class="error"></span>
                         </td>
                     </tr>
@@ -68,8 +67,14 @@ class Index(webapp2.RequestHandler):
             <input type="submit">
         </form>
 """
+
+class Index(webapp2.RequestHandler):
+    def d_form(self, error_username="", username="", error_password="", password="", error_verify="", verify="",error_email="", email=""):
+        self.response.out.write(form %{ 'error_username':error_username,'username':username,
+            'error_password':error_password,'password':password,
+            'error_verify':error_verify,'verify':verify,'error_email':error_email,'email':email})
     def get(self):
-        self.response.out.write(self.form)
+        self.d_form()
 
     def post(self):
         have_error = False
@@ -77,41 +82,33 @@ class Index(webapp2.RequestHandler):
         password = self.request.get('password')
         verify = self.request.get('verify')
         email = self.request.get('email')
-
-
-
-        params = dict(username = username,
-                      email = email)
+        error_username, error_password, error_verify, error_email = "", "", "", "",
 
         if not valid_username(username):
-            params['error_username'] = "That's not a valid username."
+            error_username = "That's not a valid username."
             have_error = True
 
         if not valid_password(password):
-            params['error_password'] = "That wasn't a valid password."
+            error_password = "That wasn't a valid password."
             have_error = True
         elif password != verify:
-            params['error_verify'] = "Your passwords didn't match."
+            error_verify = "Your passwords didn't match."
             have_error = True
 
         if not valid_email(email):
-            params['error_email'] = "That's not a valid email."
+            error_email = "That's not a valid email."
             have_error = True
-
-        if have_error:
-            #redisplay the form and params
-            self.render('signup-form.html', **params)
-        else:
-            self.redirect('/unit2/welcome?username=' + username)
+        self.d_form(error_username=error_username,error_password=error_password,error_verify=error_verify,error_email=error_email)
+        if not have_error:
+            self.redirect('/Welcome?username=' + username)
 
 class Welcome(webapp2.RequestHandler):
     def get(self):
         username = self.request.get('username')
-        if valid_username(username):
-            self.render('welcome.html', username = username)
-        else:
-            self.redirect('/unit2/signup')
+        self.response.out.write("Welcome " + username + " Congratulations!")
+
 
 app = webapp2.WSGIApplication([
     ('/', Index),
+    ('/Welcome', Welcome)
 ], debug=True)
